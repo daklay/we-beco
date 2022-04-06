cdbd47e660cb4c6aca5f55541fb7b38ce92ef7b8
mossaab amimar <0amimarmossaab@gmail.com>
2022-04-06 17:32:45 +0000

    first commit

---
+++ js/home.js
@@ -0,0 +1,25 @@
+////////////////////////////////home/////////////////////////////
+// // function hover(element) {
+// //     element.setAttribute('src', 'images/1-1.png');
+// // }
+// // function unhover(element) {
+// //     element.setAttribute('src', 'images/new1.png');
+// // }
+// // onmouseover="hover(this);" onmouseout="unhover(this);
+
+// function whover(element){
+//     switch(element.src){
+//         case "images/new1.png" :element.setAttribute('src', 'images/1-1.png');
+//         case "images/new5.png" :element.setAttribute('src', 'images/new5-2.png');
+//         case "images/new3.png" :element.setAttribute('src', 'images/new3-2.png');
+//         case "images/new4.png" :element.setAttribute('src', 'images/new4-2.png');
+                     
+//     }
+// }
+// // function lhover(element){
+
+// // }
+
+
+
+///////////////////////product page///////////////////////////////
