b972e6712aca035cd0fc1894c5a2578df342d5b5
mossaab amimar <0amimarmossaab@gmail.com>
2022-05-04 18:49:35 +0000

    last

--- js/run.js
+++ js/run.js
@@ -10,19 +10,19 @@ signInButton.addEventListener('click', () => {
 });
 
 
-const open = document.getElementById('open')
-const close = document.getElementById('close')
-let body = document.getElementsByTagName("body")[0]
-let elemnt = body.children[2]
-open.addEventListener("click", ()=>{
-    body.appendChild(elemnt)
-    body.removeChild(open)
-    body.insertBefore(close, elemnt)
-})
-close.addEventListener("click", ()=>{
-    body.removeChild(elemnt)
-    body.removeChild(close)
-    body.appendChild(open)
-})
-body.removeChild(elemnt)
-body.removeChild(close)
+// const open = document.getElementById('open')
+// const close = document.getElementById('close')
+// let body = document.getElementsByTagName("body")[0]
+// let elemnt = body.children[2]
+// open.addEventListener("click", ()=>{
+//     body.appendChild(elemnt)
+//     body.removeChild(open)
+//     body.insertBefore(close, elemnt)
+// })
+// close.addEventListener("click", ()=>{
+//     body.removeChild(elemnt)
+//     body.removeChild(close)
+//     body.appendChild(open)
+// })
+// body.removeChild(elemnt)
+// body.removeChild(close)
