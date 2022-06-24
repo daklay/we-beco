14bba301a4921fa4aceb23a4ab8286a418a2ca9a
mossaab amimar <0amimarmossaab@gmail.com>
2022-06-24 18:56:28 +0100

    src

--- js/home.js
+++ js/home.js
@@ -55,14 +55,3 @@ btnleft.addEventListener("click", ()=>{
 
 
 
-////////////////////////// * toggle menu * ///////////
-
-const primarymenu = document.querySelector(".navlist");
-const navtoggle = document.querySelector(".navtoggle");
-const test = document.querySelector(".logo1");
-
-navtoggle.addEventListener("click", ()=>{
-    primarymenu.style.opacity = "1"
-    primarymenu.classList.toggle("navlist2");
-    navtoggle.classList.toggle("navtogglex")
-})
